b5cdb12613403d6ebf3e711e7951919ed2aa18e2
mut031 <f.mutnjakovic@gmail.com>
2019-09-12 23:57:59 +0200

    Added youtube API call

--- src/app/user/search-result/search-result.component.ts
+++ src/app/user/search-result/search-result.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Item } from '../item.model';
 
 @Component({
   selector: 'app-search-result',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-result.component.scss'],
 })
 export class SearchResultComponent implements OnInit {
+  @Input() result: Item;
 
   constructor() { }
 
--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from './item.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.page.scss'],
 })
 export class UserPage implements OnInit {
+  apiUrl: string = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=iron%20maiden&key=AIzaSyDOv8hKWlin7SnRsvyy30jPOuZcGH2-USk';
 
-  constructor() { }
+  items: Item[] = [];
+  data: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  onSearch() {
+    this.http.get(this.apiUrl)
+    .subscribe(data => this.filterResults(data));
+  }
+
+  filterResults(data: any){
+    console.log(data.items)
+    this.items = data.items
+                .filter(item => item.id.kind === 'youtube#video')
+                .map(item => ({
+                  id: item.id.videoId,
+                  title: item.snippet.title,
+                  description: item.snippet.description,
+                  thumbnail: item.snippet.thumbnails.medium.url
+                }));
+  }
 }
